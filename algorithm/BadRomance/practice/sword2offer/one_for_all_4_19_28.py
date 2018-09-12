9d5f928bb18e590435a55024a0fcb79f8bf5b603
changxincheng <chengcx1019@gmail.com>
2018-09-12 17:20:53 +0800

    19-28

--- algorithm/BadRomance/practice/sword2offer/one_for_all_4_19_28.py
+++ algorithm/BadRomance/practice/sword2offer/one_for_all_4_19_28.py
@@ -13,12 +13,28 @@ from basic.tree.binary_search_tree import BinarySearchTree, BinaryTreeNode
 chapter 4 
 """
 
-"""
-19 二叉树的镜像
-"""
+
+# 19
+class Solution:
+    """
+    19 二叉树的镜像
+    """
+    # 返回镜像树的根节点
+    def mirror(self, root):
+        # write code here
+        if root is None:
+            return
+        left = root.left
+        right = root.right
+        root.left = self.mirror(right)
+        root.right = self.mirror(left)
+        return root
+
 
 """
 20 顺时针打印矩阵
+关键是推导得到循环打印的开始和停止条件，以及每个循环中每一边的开始条件，比如1X1，只有一条边需要打印，
+2X2最后一条边无需打印
 """
 
 
@@ -64,14 +80,54 @@ class PrintMatrix(object):
                 result.append(matrix[h][start])
 
 
+class Solution:
+    def __init__(self):
+        self.result = []
+
+    # matrix类型为二维列表，需要返回列表
+    def printMatrix(self, matrix):
+        # write code here
+        rows = len(matrix)
+        columns = len(matrix[0])
+        start = 0
+        while rows > 2 * start and columns > 2 * start:
+            self.print_circle(matrix, start, rows, columns)
+            start += 1
+        return self.result
+
+    def print_circle(self, matrix, start, rows, columns):
+        width = columns - 2 * start
+        height = rows - 2 * start
+        end_w = start + width - 1  # 索引总是比实际长度-1
+        end_h = start + height - 1
+
+        # left -> right
+        for w in range(start, end_w + 1):
+            self.result.append(matrix[start][w])
+        # top -> bottom
+        if end_h > start:
+            for h in range(start + 1, end_h + 1):
+                self.result.append(matrix[h][end_w])
+        # right -> left
+        if end_w > start and end_h > start:
+            for w in range(start, end_w)[::-1]:
+                self.result.append(matrix[end_h][w])
+        # bottom -> top
+        if end_h - 1 > start and end_w > start:
+            for h in range(start + 1, end_h)[::-1]:
+                self.result.append(matrix[h][start])
+
 def test_print_matrix():
     matrix = [[1, 2, 3, 4],
               [5, 6, 7, 8],
               [9, 10, 11, 12],
               [13, 14, 15, 16]]
+    matrix = [[1]]
 
     pm = PrintMatrix()
     pm.print_matrix(matrix)
+    solution = Solution()
+    solution.printMatrix(matrix)
     print(pm.result)
 
 
@@ -83,39 +139,109 @@ def test_print_matrix():
 class MinStack(object):
     # stack backed by list
     def __init__(self, ):
-        self.data = []
-        self.min = []
+        self.stack = []
+        self.mini = []
 
     def push(self, val):
         if not val:
             return
-        self.data.append(val)
-        if self.min and val < self.min[-1]:
-            self.min.append(val)
+        if not self.stack:
+            self.stack.append(val)
+            self.mini.append(val)
         else:
-            self.min.append(self.min[-1])
+            if self.mini and val < self.mini[-1]:
+                self.mini.append(val)
+            else:
+                self.mini.append(self.mini[-1])
 
     def pop(self):
-        if self.data:
-            self.min.pop()
-            return self.data.pop()
+        if self.stack:
+            self.mini.pop()
+            return self.stack.pop()
+
+    def top(self):
+        # write code here
+        if self.stack:
+            return self.stack[-1]
 
     def min(self):
-        return self.min[-1] if self.min else None
+        return self.mini[-1] if self.mini else None
+
+
+def test_mini_stack():
+    mini_stack = MinStack()
+    print(mini_stack.push(3), mini_stack.min())
 
 
 """
 22 栈的压入弹出序列
+依次弹出出栈序列首字符，开始循环
+push_seq, pop_seq,比较压栈序列和出栈序列，与出栈序列第一个字符不同字符依次进栈，两个序列弹出相同字符
+比较栈顶字符与当前出栈字符，相同则弹出字符，同时判断压栈序列是否为空，若为空，则依次比较栈的出栈元素和出栈序列是否相等
+循环结束
 """
+class IsPopOrder:
+    def is_pop_order(self, pushV, popV):
+        # write code here
+        if not pushV or not popV:
+            return False
+        stack = []
+        while popV:
+            pop_val = popV[0]
+            if stack and stack[-1] == pop_val:
+                stack.pop()
+                popV.pop(0)
+            else:
+                while pushV:
+                    if pushV[0] != pop_val:
+                        stack.append(pushV.pop(0))
+                    else:
+                        pushV.pop(0)
+                        popV.pop(0)
+                        break
+            if not pushV:
+                while stack:
+                    if stack.pop() != popV.pop(0):
+                        return False
+        return True
+
+
+def test_is_pop_order():
+    solution = IsPopOrder()
+    ret = solution.is_pop_order([1,2,3,4,5],[4,5,3,2,1])
+    print(ret)
+
 
 """
 23 从上往下打印二叉树
 即广度优先遍历二叉树
 """
+from collections import deque
+class BFS(object):
+
+    def __init__(self, ):
+        self.result = []
+
+    def bfs(self, root: BinaryTreeNode):
+        if root is None:
+            return []
+
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            self.result.append(node.val)
+
 
 """
 24 二叉树的后序遍历序列
-判断一个序列是不是给定树的后续遍历，关注后序遍历的特性
+判断一个序列是不是给定二叉搜索树的后续遍历，关注后序遍历的特性
+每个序列的最后一个元素是根节点，左子比根节点都要小，右子比根节点都大
 """
 
 
@@ -134,6 +260,26 @@ def is_post_order(order: list):
     is_left = True if left == 0 else is_post_order(order[:left])  # 左子序列为空, 因而left = 0
     is_right = True if left == right else is_post_order(order[left: right])  # 右子树为空，因而left==right
     return is_left and is_right
+
+
+class IsPostOrder(object):
+
+    def VerifySquenceOfBST(self, sequence):
+        # write code here
+        if not sequence and len(sequence) == 0:
+            return False
+        root = sequence[-1]
+        left = 0
+        while sequence[left] < root:
+            left += 1
+        right = left
+        while right < len(sequence) - 1:
+            if sequence[right] < root:
+                return False
+            right += 1
+        is_left = True if left == 0 else self.VerifySquenceOfBST(sequence[:left])
+        is_right = True if left == right else self.VerifySquenceOfBST(sequence[left: right])
+        return is_left and is_right
 
 
 """
@@ -171,6 +317,32 @@ class FindPathSum(object):
         path.pop()
 
 
+class FindSumPath(object):
+    def __init__(self):
+        self.ret = []
+
+    # 返回二维列表，内部每个列表表示找到的路径
+    def find_path(self, root, expect_number):
+        if root is None:
+            return self.ret
+        path = []
+        curr_sum = 0
+
+        def _find_path(root, expected_sum, path, current_num):
+            is_leaf = root.left is None and root.right is None
+            current_num += root.val
+            path.append(root.val)
+            if current_num == expected_sum and is_leaf:
+                self.ret.append(path[:])
+            if root.left:
+                _find_path(root.left, expected_sum, path, current_num)
+            if root.right:
+                _find_path(root.right, expected_sum, path, current_num)
+            current_num -= root.val  # 回溯
+            path.pop()
+        _find_path(root, expect_number, path, curr_sum)
+
+
 def test_find_path():
     from basic.tree.binary_search_tree import BinaryTreeNode
     root = BinaryTreeNode(10)
@@ -187,14 +359,12 @@ def test_find_path():
     fps.find_path(root, 22)
 
 
-"""
-26 复杂链表的复制
-由于每个节点的额外指向不能确定是在前面还是后面，因而如果不采用额外的空间或者经历一次遍历之后，指向的节点可能还没有被复制
-"""
-
-
+# 26
 class CloneComplexLinkedList(object):
-
+    """
+    26 复杂链表的复制
+    由于每个节点的额外指向不能确定是在前面还是后面，因而如果不采用额外的空间或者经历一次遍历之后，指向的节点可能还没有被复制
+    """
     def __init__(self, ):
         pass
 
@@ -242,54 +412,56 @@ class CloneComplexLinkedList(object):
 # test refer test_cloen_complex_linkedlist
 
 
-"""
-27 二叉搜索树与双向链表
-将二叉搜索树转换成一个排序的双向链表
-前序遍历可将搜索二叉树从小到大遍历
-"""
-
-
+# 27
 class TransferBstToDoubleLinkedList(object):
-    
+    """
+    27 二叉搜索树与双向链表
+    将二叉搜索树转换成一个排序的双向链表
+    前序遍历可将搜索二叉树从小到大遍历
+    """
     def __init__(self, ):
         pass
 
-    def transfer(self, tree):
-        if tree is None:
+    # 输入为数的根节点
+    def transfer(self, root):
+        if root is None:
             return
-        last_node = None
-        root = tree.root
-        self._construce(root, last_node)
+        last_node = None  # 当前构建的链表最后一个节点
+        last_node = self._construct(root, last_node)
 
-        while last_node is not None and last_node.left is not None:
+        while last_node is not None and last_node.left is not None:  # 将链表指针指向最左边的节点
             last_node = last_node.left
 
         return last_node
 
-    def _construce(self, node, last_node):
-        current_node = node
-        if current_node.left is not None:
-            self._construce(current_node.left, last_node)
+    def _construct(self, root, last):
+        curr = root
+        if curr.left is not None:
+            last = self._construct(curr.left, last)
 
-        current_node.left = last_node
-        if last_node is not None:
-            last_node.next = current_node
-        last_node = current_node
-
-        if current_node.right is not None:
-            self._construce(current_node.right, last_node)
-
-
-"""
-28 字符串的排列
-"""
+        if last is not None:
+            last.right = curr
+        curr.left = last
+        last = curr
+        if curr.right is not None:
+            last = self._construct(curr.right, last)
+        return last
 
 
+
+
+import itertools
 class Permutation(object):
-    
+    """
+    28 字符串的排列
+    1 permutation只能处理不含重复字符的情况，
+    2 而_Permutation可以处理更一般的情况
+    3 repeat_char是一种突然就实现了的方法
+    """
     def __init__(self, ):
         self.str_perm = []
         self.result = []
+        self.ret = []
 
     def permutation(self, chars: str):
         for item in chars:
@@ -301,14 +473,59 @@ class Permutation(object):
                 self.result.append(''.join(self.str_perm))
             self.str_perm.pop()
 
+    def sub(self, string, index, new_char):
+        new = []
+        for s in string:
+            new.append(s)
+        new[index] = new_char
+        return ''.join(new)
+
+
+    def _Permutation(self, ss):
+        for i, item in enumerate(ss):
+            s = self.sub(ss, i, '')
+            self.str_perm.append(item)
+            if len(s) > 0:
+                self._Permutation(s)
+            else:
+                print(self.str_perm)
+                self.result.append(self.str_perm[:])
+            self.str_perm.pop()
+
+    def Permutation(self, ss):
+        # write code here
+        if not ss:
+            return []
+        self._Permutation(ss)
+        print(self.result)
+        for i in self.result:
+            if "".join(i) not in self.ret:
+                self.ret.append("".join(i))
+        return self.ret
+
+    def repeat_char(self, ss):
+        result = []
+        if not ss:
+            return []
+        else:
+            res = itertools.permutations(ss)
+            for i in res:
+                if "".join(i) not in result:
+                    result.append("".join(i))
+        return result
+
 
 def test_permutation():
     chars = 'abc'
     p = Permutation()
-    p.permutation(chars)
+    p.Permutation(chars)
     print(p.result)
     
 
 if __name__ == '__main__':
     # test_find_path()
+    # test_permutation()
+    # test_print_matrix()
+    # test_mini_stack()
+    # test_is_pop_order()
     test_permutation()
